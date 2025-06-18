d90bd2a2453121d26a7eae8966ea71dab804ab75
belze79 <zararaz2002@gmail.com>
2025-06-18 17:32:42 +0200

    add builds .yml 2

--- tris_main.py
+++ tris_main.py
@@ -4,6 +4,8 @@ from session import session
 from view_py import set_window
 from controller_py import music_loop
 
+print('print di log prima dell\'avvio dell\'interfaccia')
+
 session.root_config.root = Tk()
 session.root_config.root.title('Tic Tac Toe')
 session.root_config.root.geometry(f'{session.root_config.width}x{session.root_config.height}+{1300 - session.root_config.width}+80')
@@ -17,4 +19,8 @@ set_window()
 
 music_loop()
 
+print('print di log dopo il set dell\'interfaccia')
+
 session.root_config.root.mainloop()
+
+print('print di log alla chiusura dell\'app')
